66230971592ac051372e8b0e97b66cf5d8605e7a
MatheusHorning <wojcikhorning@gmail.com>
2026-04-16 10:56:40 -0300

    Remove arquivo de anagramas

--- solucoes.py
+++ solucoes.py
@@ -1,6 +1,25 @@
 def sao_anagramas(string1, string2):
-    # TODO: Implementar a lógica
-    pass
+    string1 = string1.replace(" ", "")
+    string2 = string2.replace(" ", "")
+    
+    string1 = string1.lower()
+    string2 = string2.lower()
+    
+    lista1 = list(string1)
+    lista2 = list(string2)
+    
+    lista1.sort()
+    lista2.sort()
+    
+    if lista1 == lista2:
+        return True
+    else:
+        return False
+
+
+print(sao_anagramas("feio", "bunito"))
+print(sao_anagramas("bota ", "toba"))
+print(sao_anagramas("tijolo", "Lojito"))
 
 def cifra_de_cesar(texto, deslocamento):
     # TODO: Implementar a lógica
