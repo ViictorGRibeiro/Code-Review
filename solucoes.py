01dacd027d3270b7e55ec21f607dedb3d4cfd7a2
LuNK1108 <lucs.os1108@gmail.com>
2026-04-16 10:43:59 -0300

    adicionei o codigo da cifra de cesar

--- solucoes.py
+++ solucoes.py
@@ -3,9 +3,24 @@ def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
-    # TODO: Implementar a lógica
-    pass
+    resultado = ""
+
+    for char in texto:
+        if char.isalpha():  # verifica se é letra
+            # define base (maiúscula ou minúscula)
+            base = ord('A') if char.isupper() else ord('a')
+            
+            # aplica o deslocamento
+            nova_letra = (ord(char) - base + deslocamento) % 26 + base
+            
+            resultado += chr(nova_letra)
+        else:
+            # mantém espaços e símbolos
+            resultado += char
+
+    return resultado
 
 def encontrar_maior_palavara(frase):
     # TODO: Implementar a lógica
     pass
+print("oi")
