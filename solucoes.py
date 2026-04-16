3cd57a7abc90750f78ed51fc8eb91005d9b12705
ViictorGRibeiro <vg790257@gmail.com>
2026-04-16 10:19:11 -0300

    Adicionando o codigo para ser alterado pelos desenvolveldores

---
+++ solucoes.py
@@ -0,0 +1,11 @@
+def sao_anagramas(string1, string2):
+    # TODO: Implementar a lógica
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    # TODO: Implementar a lógica
+    pass
+
+def encontrar_maior_palavara(frase):
+    # TODO: Implementar a lógica
+    pass
